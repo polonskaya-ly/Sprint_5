775b0976ada9faf50d1495289e8a5f6f48c40891
polonskaya-ly <korabli-piter@yandex.ru>
2024-06-28 10:37:24 +0300

    After second review_2

--- tests/test_move_to_constructor_headers.py
+++ tests/test_move_to_constructor_headers.py
@@ -17,8 +17,8 @@ class TestMoveToConstructorHeaders:
     def test_move_to_constructor_headers_bread(self):
         self.driver.find_element(By.XPATH, Locators.topping_button).click()
         self.driver.find_element(By.XPATH, Locators.bread_button).click()
-        constructor_button = self.driver.find_element(By.XPATH, Locators.bread_button).get_attribute('class')
-        assert constructor_button == Locators.active_constructor_header
+        ingredient_button = self.driver.find_element(By.XPATH, Locators.bread_button).get_attribute('class')
+        assert ingredient_button == Locators.active_constructor_header
 
 
 
