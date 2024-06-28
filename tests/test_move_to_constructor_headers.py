0fe2d83344ffb3c1fddb5e05134140cbf21d77e3
polonskaya-ly <korabli-piter@yandex.ru>
2024-06-28 10:39:19 +0300

    After second review_3

--- tests/test_move_to_constructor_headers.py
+++ tests/test_move_to_constructor_headers.py
@@ -6,13 +6,13 @@ from ..locators import Locators
 class TestMoveToConstructorHeaders:
     def test_move_to_constructor_headers_sauce(self):
         self.driver.find_element(By.XPATH, Locators.sauce_button).click()
-        header_main = self.driver.find_element(By.XPATH, Locators.sauce_button).get_attribute('class')
-        assert header_main == Locators.active_constructor_header
+        ingredient_button = self.driver.find_element(By.XPATH, Locators.sauce_button).get_attribute('class')
+        assert ingredient_button == Locators.active_constructor_header
 
     def test_move_to_constructor_headers_topping(self):
         self.driver.find_element(By.XPATH, Locators.topping_button).click()
-        header_main = self.driver.find_element(By.XPATH, Locators.topping_button).get_attribute('class')
-        assert header_main == Locators.active_constructor_header
+        ingredient_button = self.driver.find_element(By.XPATH, Locators.topping_button).get_attribute('class')
+        assert ingredient_button == Locators.active_constructor_header
 
     def test_move_to_constructor_headers_bread(self):
         self.driver.find_element(By.XPATH, Locators.topping_button).click()
